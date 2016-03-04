9fdc64f0e311a9f91804d7229909c7b5c8999865
Jelani Thompson <jelanithompson@hotmail.com>
2016-03-03 19:46:52 -0500

    Can now successfully display icons

--- js/weather.js
+++ js/weather.js
@@ -3,10 +3,10 @@
 
 const Brampton = "http://api.openweathermap.org/data/2.5/weather?lat=43.72349010000001&lon=-79.714882&appid=d92242a2a0f022fbdfc75b0c9499bbec";
 
-//Add elements from DOM
 let $city = $("#city");
 let $weather = $("#weather");
 let $temperature = $("#temperature");
+let $weatherIcon = $("#weatherIcon");
 
 //Output data from URL via JSON
 //Wrap in function so it calls when DOM elements are ready
@@ -30,6 +30,17 @@ $(function() {
         let weather = json.weather[0].main;
         let temperature = Math.round(json.main.temp);
         console.log("The weather in " + cityName + " is " + weather + " and the temperature is " + temperature + " °C");
+
+        //Output appropriate weather ID
+        for (var i = 0; i < weatherIDs.length; i++) {
+          if (weatherIDs[i].rangeMin || weatherIDs[i].rangeMax == json.weather[0].id) {
+            console.log("The weather ID is " + json.weather[0].id + " (" + weather + ")");
+
+            //Now put the appropriate weather icon for ID
+            $weatherIcon.html("<i class = " + "'" + weatherIDs[i].icon + "'" + "></i>");
+            console.log("<i class = " + "'" + weatherIDs[i].icon + "'" + "></i>");
+          }
+        }
 
         //Output values in HTML
         $city.html(cityName);
@@ -86,7 +97,3 @@ let weatherIDs = [
     rangeMax: 804
   }
 ];
-
-//Determine what name to pick by matching IDs
-
-//If ID matches range, match weather group
