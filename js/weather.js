4b555f1639213be52f343c33b1152f2f2b6ff6bb
Jelani Thompson <jelanithompson@hotmail.com>
2016-03-06 13:35:12 -0500

    Finished app function and user stories

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,11 @@
 //const Brampton = "http://api.openweathermap.org/data/2.5/weather?lat=43.72349010000001&lon=-79.714882&appid=d92242a2a0f022fbdfc75b0c9499bbec";
 
 let $city = $("#city");
-let $weather = $("#weather");
+let $description = $("#description");
 let $temperature = $("#temperature");
 let $weatherIcon = $("#weatherIcon");
+let $celsius = $("#celsius");
+let $fahrenheit = $("#fahrenheit");
 
 //Output data from URL via JSON
 //Wrap in function so it calls when DOM elements are ready
@@ -18,7 +20,7 @@ $(function() {
     navigator.geolocation.getCurrentPosition(function(location) {
       let latitude = location.coords.latitude;
       let longitude = location.coords.longitude;
-      const coordinates = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=d92242a2a0f022fbdfc75b0c9499bbec&units=metric";
+      let coordinates = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=d92242a2a0f022fbdfc75b0c9499bbec&units=metric";
 
       //Output data from URL via JSON
       $.getJSON(coordinates, function(json) {
@@ -28,7 +30,11 @@ $(function() {
         //Output API data (should eventually evolve into outputting user's location and weather)
         let cityName = json.name;
         let weather = json.weather[0].main;
+        let description = json.weather[0].description;
         let temperature = Math.round(json.main.temp);
+        let celsius = Math.round(json.main.temp);
+        let fahrenheit = Math.round(json.main.temp * 9 / 5 + 32);
+
         console.log("The weather in " + cityName + " is " + weather + " and the temperature is " + temperature + " °C");
 
         //Output appropriate weather ID
@@ -36,20 +42,30 @@ $(function() {
 
         //Now put the appropriate weather icon for ID
         $weatherIcon.html("<i class=" + "'" + "wi wi-owm-" + json.weather[0].id + "'" + "></i>");
-        //console.log("<i class=" + "'" + "wi wi-owm-" +  json.weather[0].id + "'" + "></i>");
 
         //Output values in HTML
         $city.html(cityName);
-        //Change to be description
-        $weather.html(weather);
-        $temperature.html(temperature + " °C");
+        $description.html(description);
+        $temperature.html(temperature);
+
+        //Change html to be °C or °F value when you click either one
+        $fahrenheit.on('click', function() {
+          $(".wi-fahrenheit").addClass("active");
+          $(".wi-fahrenheit").removeClass("inactive");
+          $(".wi-celsius").removeClass("active");
+          $(".wi-celsius").addClass("inactive");
+          $temperature.html(fahrenheit);
+        });
+
+        $celsius.on('click', function() {
+          $(".wi-celsius").addClass("active");
+          $(".wi-celsius").removeClass("inactive");
+          $(".wi-fahrenheit").removeClass("active");
+          $(".wi-fahrenheit").addClass("inactive");
+          $temperature.html(celsius);
+        });
+
       });
     });
   }
-
-  //Convert Celsius to Fahrenheit (ternary operator maybe?)
-  function temperatureConversion() {
-
-  }
-
 });
